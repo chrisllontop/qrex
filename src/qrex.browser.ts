98fea644a6cf45baf0763ab925c94d81af9e64a9
Christian Llontop <me@chrisllontop.com>
2025-01-24 23:08:19 -0500

    Add renderOptions parameter to Qrex browser

--- src/qrex.browser.ts
+++ src/qrex.browser.ts
@@ -1,7 +1,7 @@
 import { QrexBase } from "./qrex.base";
 import { RendererCanvas } from "./renderer/canvas";
 import { RendererSvgTag } from "./renderer/svg-tag";
-import type { QrexOptions, QrContent } from "./types/qrex.type";
+import type { QrexOptions, QrContent, RenderOptions } from "./types/qrex.type";
 
 export class Qrex extends QrexBase {
   private readonly rendererCanvas: RendererCanvas;
@@ -12,15 +12,15 @@ export class Qrex extends QrexBase {
     this.rendererCanvas = new RendererCanvas(canvas);
   }
 
-  public toCanvas() {
-    return this.render(this.rendererCanvas.render.bind(this.rendererCanvas));
+  public toCanvas(renderOptions?: RenderOptions) {
+    return this.render(this.rendererCanvas.render.bind(this.rendererCanvas), renderOptions);
   }
 
-  public toDataURL() {
-    return this.render(this.rendererCanvas.renderToDataURL.bind(this.rendererCanvas));
+  public toDataURL(renderOptions?: RenderOptions) {
+    return this.render(this.rendererCanvas.renderToDataURL.bind(this.rendererCanvas), renderOptions);
   }
 
-  public toString() {
-    return this.render(this.rendererSvgTag.render.bind(this.rendererSvgTag));
+  public toString(renderOptions?: RenderOptions) {
+    return this.render(this.rendererSvgTag.render.bind(this.rendererSvgTag), renderOptions);
   }
 }
